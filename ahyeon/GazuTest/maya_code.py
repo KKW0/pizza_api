9a5e02c2cebace4422dc4eb18cf44290e12281fa
Ahyeon Jo <keiel0326@gmail.com>
2023-02-01 16:05:36 +0900

    클래스로 만들었어요

---
+++ ahyeon/GazuTest/maya_code.py
@@ -0,0 +1,14 @@
+# coding:utf8
+import maya.cmds as mc
+
+# # Example
+# import sys
+# sys.path.append("/home/rapa/TEST/git/pizza/ahyeon/GazuTest")
+# import main
+# from main import GetModelInformation
+# reload(get_model)
+#
+# gmi = GetModelInformation()
+# gmi.model = 'pCylinder1'
+# my_dict = gmi.get_hierarchy_dict('joint')
+# gmi.make_json(my_dict)

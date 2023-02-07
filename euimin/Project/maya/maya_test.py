ab9549f546de8536153937b5eb4a99978745c598
EuiMin Lee <coelha9160@gmail.com>
2023-02-07 17:15:31 +0900

    add files

---
+++ euimin/Project/maya/maya_test.py
@@ -0,0 +1,13 @@
+# import sys
+# sys.path.append('/home/rapa/git/pizza/euimin/Project/maya')
+# import maya_test
+
+import maya.cmds as cmds
+
+cmds.polySphere()
+
+# path = "/home/rapa/dd.fbx"
+# cmds.file(path, i=1, type="FBX", ignoreVersion=1, ra=1,
+#         mergeNamespacesOnClash=0, rpr="cam", options="fbx",
+#         pr=1, importTimeRange="combine"
+# )

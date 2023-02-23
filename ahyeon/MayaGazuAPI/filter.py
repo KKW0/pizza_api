da873f8d25ca5b49136d47f0abcebb4ad8fdffdc
Ahyeon Jo <keiel0326@gmail.com>
2023-02-23 14:43:47 +0900

    임시저장

---
+++ ahyeon/MayaGazuAPI/filter.py
@@ -0,0 +1,9 @@
+#coding:utf8
+import gazu
+import pprint as pp
+
+
+class Filter:
+    def __init__(self):
+        pass
+    # 정태님 힘내용
--- ahyeon/MayaGazuAPI/login.py
+++ ahyeon/MayaGazuAPI/login.py
@@ -1,6 +1,8 @@
-# import gazu
-#
-#
-# def log_in():
-#     gazu.client.set_host("http://192.168.3.116/api")
-#     gazu.log_in("pipeline@rapa.org", "netflixacademy")
+#coding:utf8
+import gazu
+
+
+class LogIn:
+    def __init__(self):
+        gazu.client.set_host("http://192.168.3.116/api")
+        gazu.log_in("pipeline@rapa.org", "netflixacademy")

394b37de6e537fcbc11f60b7b740d7acb7335ca3
Br Lee <mmuerod@gmail.com>
2023-02-27 11:38:38 +0900

    수정함

--- br/load.py
+++ br/load.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 # import maya
 from logging import *
 import os
@@ -5,30 +6,28 @@ import gazu
 import construct_full_path
 
 
-
-
 class Load:
+    """
+    Gazu 프로젝트 관리 시스템에서 선택한 작업에 대한 Maya 로더를 나타냅니다.
 
+    Attributes:
+        selected_task (dict): Gazu 프로젝트 관리 시스템에서 선택한 작업을 나타내는 사전입니다.
+        user_name (str): 인증된 사용자의 전체 이름입니다.
+        logger (Logger): 작업 파일 로그를 만드는 로거 개체입니다.
+        auth (Auth): Gazu API에 액세스하기 위한 인증 개체입니다.
+
+    Methods:
+        new_maya_working_file(name='main', comment='') -> dict:
+            선택한 작업에 대해 Gazu 프로젝트 관리 시스템에 새 Maya 작업 파일을 생성하고 새 작업 파일을 나타내는 사전을 반환합니다.
+    """
     def __init__(self, task):
-        # self.logger = Logger()
-        # self.auth = Auth()
         self.user_name = self.auth.user.get('full_name')
         self.selected_task = task
+        self.logger = Logger()
+        self.auth = Auth()
         self.new_maya_working_file()
 
-    def construct_full_path(file: dict):
-        """
-        output file이나 working file의 딕셔너리를 받아서 확장자까지 연결된 full path를 반환
-
-        Args:
-            file(dict):working file 혹은 output file dict
-
-        Returns:
-            str: file의 실제 절대경로
-                    {dir_name}/{file_name}.{extension}
-                확장자가 레스터 이미지 확장자인 경우, padding을 포함
-                    {dir_name}/{file_name}.####.{extension}
-        """
+    def construct_full_path(file):
         path = file.get('path')
         file_type = file.get('type')
         padding = '.'
@@ -41,19 +40,11 @@ class Load:
             if ext in ['exr', 'dpx', 'jpg', 'jpeg', 'png', 'tga']:
                 padding = '.####.'
         else:
-            raise Exception('파일 딕셔너리가 아님')
+            raise ValueError('file must be a WorkingFile or OutputFile dictionary')
         return path + padding + ext
 
-    def new_maya_working_file(self, name='main', comment='') -> dict:
-        """
+    def new_maya_working_file(self, name='main', comment=''):
 
-        Args:
-            name(str, optional): working file dict의 이름, 기본값 "main"
-            comment(str, optional): working file dict의 설명
-
-        Returns:
-            dict: create working file dict
-        """
         maya = gazu.files.get_software_by_name('maya')
         user = gazu.client.get_current_user()
         working_file = gazu.files.new_working_file(self.selected_task, name=name, comment=comment,
@@ -68,9 +59,9 @@ class Load:
 
         for file in os.listdir(root_dir):
             if file == file_name:
-                raise WorkingFileExistsError("Already exists working file")
+                raise FileExistsError("Working file already exists")
 
-        os.system(f"touch {working_file_path}")
+        os.system("touch {0}".format(working_file_path))
         self.logger.create_working_file_log(self.user_name, working_file_path)
 
         return working_file

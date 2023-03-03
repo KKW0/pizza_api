6cc858fd0cde15110d61eb78e1ceff6b729fb701
Ahyeon Jo <keiel0326@gmail.com>
2023-03-03 09:13:57 +0900

    ah, python 폴더만 남김

--- euimin/test2.py
+++
@@ -1,6 +0,0 @@
-import maya.cmds as mc
-
-mesh=mc.ls(sl=1)
-meshShape=mc.listRelatives(mesh,s=1)[0]
-skin=mc.listConnections(meshShape,type="skinCluister")[0]
-skinJNT=mc.listConnections(skin+'.matrix')
--- jeongtae/pythonProject/test_filtering.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from filtering import Fillter
-
-class TestFillter(TestCase):
-    def setUp(self):
-        self.fi = Fillter
-
-

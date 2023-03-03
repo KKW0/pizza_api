02c3923704114ac919c670634de4dd77fe8bb19b
kyoungwook <kang5349@gmail.com>
2023-03-03 09:14:01 +0900

    최신화2

--- ahyeon/MayaGazuAPI/login.py
+++
@@ -1,8 +0,0 @@
-#coding:utf8
-import gazu
-
-
-class LogIn:
-    def __init__(self):
-        gazu.client.set_host("http://192.168.3.116/api")
-        gazu.log_in("pipeline@rapa.org", "netflixacademy")
--- python/tmp_file/login_tmp.py
+++
@@ -1,6 +0,0 @@
-# import gazu
-#
-#
-# def log_in():
-#     gazu.client.set_host("http://192.168.3.116/api")
-#     gazu.log_in("pipeline@rapa.org", "netflixacademy")

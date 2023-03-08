6568c7b4c3227069869fec2160680650813d203f
kyoungwook <kang5349@gmail.com>
2023-03-08 11:31:09 +0900

    ddd

--- kkw/PJT_2st/Login.py
+++ kkw/PJT_2st/Login.py
@@ -4,14 +4,12 @@ import os
 import sys
 
 from login_kkw import Auth_br
-# from Maya_Api import MainWindow
 from PySide2 import QtWidgets, QtCore, QtUiTools
 
 class MainLogin(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainLogin, self).__init__()
 
-        # self.main_window = MainWindow()
         self.Login = None
         self.user_list_start = None
         ui_path = os.path.expanduser('/home/rapa/git/pizza/kkw/PJT_2st/Login.ui')
@@ -22,23 +20,15 @@ class MainLogin(QtWidgets.QMainWindow):
 
         ui_file.close()
 
-        # self.ui.show()
+        self.login = Auth_br()
+    # ----------------------------------------------------------------------------------------------
 
-
-        # self.ui.Login_Button.clicked.connect(self.login_button_clicked)
         self.Host_Box = self.ui.findChild(QtWidgets.QLineEdit, "Host_Box")
 
         self.ID_Box = self.ui.findChild(QtWidgets.QLineEdit, "ID_Box")
         self.PW_Box = self.ui.findChild(QtWidgets.QLineEdit, "PW_Box")
 
-        self.login = Auth_br()
-        # value = self.login.load_setting()
-        # if value and value['auto_login'] and value['valid_host'] and value['valid_user']:
-        #     self.ui.hide()
-            # self.main_window.ui.show()
-
-
-
+    # ----------------------------------------------------------------------------------------------
 
 def main():
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)

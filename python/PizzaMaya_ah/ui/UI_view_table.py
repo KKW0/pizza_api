5ecf052329eaa1570717acce588941a8f25ca15d
Jeongtae Park <zhvkzk3@gmail.com>
2023-03-13 13:40:32 +0900

    저장

--- python/PizzaMaya_ah/ui/UI_view_table.py
+++ python/PizzaMaya_ah/ui/UI_view_table.py
@@ -2,10 +2,21 @@
 
 from UI_model import CustomTableModel
 from UI_model import CustomTableModel2
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore, QtGui
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QFont
-from PySide2.QtWidgets import QTableView
+from PySide2.QtWidgets import QTableView, QComboBox, QStyledItemDelegate
+
+
+class WrapDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        text = index.data(QtCore.Qt.DisplayRole)
+        document = QtGui.QTextDocument()
+        document.setHtml(text)
+        document.setTextWidth(option.rect.width())
+        option.text = ""
+        painter.translate(option.rect.x(), option.rect.y())
+        document.drawContents(painter)
 
 
 class Table(QTableView):
@@ -15,6 +26,7 @@ class Table(QTableView):
     def __init__(self, data):
         QTableView.__init__(self)
 
+        self.setSortingEnabled(True)
         self.model = CustomTableModel(data)
         self.setModel(self.model)
 
@@ -30,16 +42,57 @@ class Table(QTableView):
         self.verticalHeader().setDefaultAlignment(Qt.AlignVCenter)
 
         # QTableView Headers
-        self.horizontal_header = self.horizontalHeader()
+        self.combo_box = QComboBox(self.horizontalHeader())
+        self.combo_box.setGeometry(0, 0, 100, 30)
+        self.combo_box.addItems(['All', 'Option 1', 'Option 2'])
+        self.combo_box.currentIndexChanged.connect(self.filter_list)
+
+        self.combo_box2 = QComboBox(self.horizontalHeader())
+        self.combo_box2.setGeometry(100, 0, 100, 30)
+        self.combo_box2.addItems(['All', 'Option A', 'Option B'])
+        self.combo_box2.currentIndexChanged.connect(self.filter_list)
+
+        # self.horizontal_header = self.horizontalHeader()
         self.vertical_header = self.verticalHeader()
 
         self.horizontalHeader().setMinimumSectionSize(50)
         self.verticalHeader().setMinimumSectionSize(50)
 
-        self.horizontal_header.setStretchLastSection(True)
+        self.horizontalHeader().setStretchLastSection(True)
 
         self.setStyleSheet("background-color: #353535; selection-background-color: gray;")
         self.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
+
+        delegate = WrapDelegate(self.combo_box)
+        self.horizontalHeader().setItemDelegateForColumn(0, delegate)
+
+        delegate2 = WrapDelegate(self.combo_box2)
+        self.horizontalHeader().setItemDelegateForColumn(1, delegate2)
+
+    def filter_list(self, index):
+        option = self.combo_box.currentText()
+        option2 = self.combo_box2.currentText()
+
+        proxy_model = QtCore.QSortFilterProxyModel()
+        proxy_model.setSourceModel(self.model)
+
+        if option != 'All':
+            proxy_model.setFilterRegExp('^{}$'.format(option))
+            proxy_model.setFilterKeyColumn(0)
+            self.combo_box2.setDisabled(False)
+        else:
+            self.combo_box2.setCurrentIndex(0)
+            option2 = 'All'
+            self.combo_box2.setDisabled(True)
+
+        if option2 != 'All':
+            proxy_model2 = QtCore.QSortFilterProxyModel()
+            proxy_model2.setSourceModel(proxy_model)
+            proxy_model2.setFilterRegExp('^{}$'.format(option2))
+            proxy_model2.setFilterKeyColumn(1)
+            self.setModel(proxy_model2)
+        else:
+            self.setModel(proxy_model)
 
 
 class Table2(QTableView):
--- python/PizzaMaya_ah/ui/filter_testtttt.py
+++ python/PizzaMaya_ah/ui/filter_testtttt.py
@@ -2,15 +2,15 @@
 # from PySide2 import QtWidgets, QtCore, QtGui
 #
 #
-# class WrapDelegate(QStyledItemDelegate):
-#     def paint(self, painter, option, index):
-#         text = index.data(QtCore.Qt.DisplayRole)
-#         document = QtGui.QTextDocument()
-#         document.setHtml(text)
-#         document.setTextWidth(option.rect.width())
-#         option.text = ""
-#         painter.translate(option.rect.x(), option.rect.y())
-#         document.drawContents(painter)
+class WrapDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        text = index.data(QtCore.Qt.DisplayRole)
+        document = QtGui.QTextDocument()
+        document.setHtml(text)
+        document.setTextWidth(option.rect.width())
+        option.text = ""
+        painter.translate(option.rect.x(), option.rect.y())
+        document.drawContents(painter)
 #
 #
 # class MainWindow(QMainWindow):
@@ -28,10 +28,10 @@
 #         self.combo_box.addItems(['All', 'Option 1', 'Option 2'])
 #         self.combo_box.currentIndexChanged.connect(self.filter_list)
 #
-#         self.combo_box2 = QComboBox(self.table_view.horizontalHeader())
-#         self.combo_box2.setGeometry(100, 0, 100, 30)
-#         self.combo_box2.addItems(['All', 'Option A', 'Option B'])
-#         self.combo_box2.currentIndexChanged.connect(self.filter_list)
+        # self.combo_box2 = QComboBox(self.table_view.horizontalHeader())
+        # self.combo_box2.setGeometry(100, 0, 100, 30)
+        # self.combo_box2.addItems(['All', 'Option A', 'Option B'])
+        # self.combo_box2.currentIndexChanged.connect(self.filter_list)
 #
 #         self.model = QtGui.QStandardItemModel()
 #         self.model.appendRow([QtGui.QStandardItem('Option 1'), QtGui.QStandardItem('Option A'), QtGui.QStandardItem('2023-03-08')])
@@ -46,37 +46,37 @@
 #         self.table_view.setAlternatingRowColors(True)
 #         self.setCentralWidget(self.table_view)
 #
-#         delegate = WrapDelegate(self.combo_box)
-#         self.table_view.horizontalHeader().setItemDelegateForColumn(0, delegate)
+        delegate = WrapDelegate(self.combo_box)
+        self.table_view.horizontalHeader().setItemDelegateForColumn(0, delegate)
 #
-#         delegate2 = WrapDelegate(self.combo_box2)
-#         self.table_view.horizontalHeader().setItemDelegateForColumn(1, delegate2)
+        delegate2 = WrapDelegate(self.combo_box2)
+        self.table_view.horizontalHeader().setItemDelegateForColumn(1, delegate2)
 #
 #
-#     def filter_list(self, index):
-#         option = self.combo_box.currentText()
-#         option2 = self.combo_box2.currentText()
-#
-#         proxy_model = QtCore.QSortFilterProxyModel()
-#         proxy_model.setSourceModel(self.model)
-#
-#         if option != 'All':
-#             proxy_model.setFilterRegExp('^{}$'.format(option))
-#             proxy_model.setFilterKeyColumn(0)
-#             self.combo_box2.setDisabled(False)
-#         else:
-#             self.combo_box2.setCurrentIndex(0)
-#             option2 = 'All'
-#             self.combo_box2.setDisabled(True)
-#
-#         if option2 != 'All':
-#             proxy_model2 = QtCore.QSortFilterProxyModel()
-#             proxy_model2.setSourceModel(proxy_model)
-#             proxy_model2.setFilterRegExp('^{}$'.format(option2))
-#             proxy_model2.setFilterKeyColumn(1)
-#             self.table_view.setModel(proxy_model2)
-#         else:
-#             self.table_view.setModel(proxy_model)
+    def filter_list(self, index):
+        option = self.combo_box.currentText()
+        option2 = self.combo_box2.currentText()
+
+        proxy_model = QtCore.QSortFilterProxyModel()
+        proxy_model.setSourceModel(self.model)
+
+        if option != 'All':
+            proxy_model.setFilterRegExp('^{}$'.format(option))
+            proxy_model.setFilterKeyColumn(0)
+            self.combo_box2.setDisabled(False)
+        else:
+            self.combo_box2.setCurrentIndex(0)
+            option2 = 'All'
+            self.combo_box2.setDisabled(True)
+
+        if option2 != 'All':
+            proxy_model2 = QtCore.QSortFilterProxyModel()
+            proxy_model2.setSourceModel(proxy_model)
+            proxy_model2.setFilterRegExp('^{}$'.format(option2))
+            proxy_model2.setFilterKeyColumn(1)
+            self.table_view.setModel(proxy_model2)
+        else:
+            self.table_view.setModel(proxy_model)
 #
 #
 #

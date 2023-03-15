ec04295faef06280b196232797cf2fac942a1223
EuiMin Lee <coelha9160@gmail.com>
2023-03-15 16:51:28 +0900

    unittest_login

--- python/PizzaMaya_ah/test/test_login.py
+++ python/PizzaMaya_ah/test/test_login.py
@@ -1,7 +1,6 @@
 #coding:utf8
 from unittest import TestCase
 from PizzaMaya_ah.code.login import LogIn
-import json
 import os
 
 
@@ -44,77 +43,54 @@ class TestLogIn(TestCase):
 
         self.login.host = self.valid_url
         self.login.connect_host()
-        self.assertTrue(self.login._valid_host)
+        self.assertTrue(self.login.valid_host)
+        self.assertTrue(self.login.connect_host())
 
     def test_log_in(self):
-        # self.assertFalse(self.login.connect_host(self.invalid_url))
-        self.assertTrue(self.login.connect_host(self.valid_url))
+        self.login.host = self.valid_url
+        self.login.connect_host()
+        self.login.user_id = self.invalid_id
+        self.login.user_pw = self.invalid_pw
         with self.assertRaises(ValueError):
-            self.login.log_in(self.invalid_id, self.invalid_pw)
+            self.login.log_in()
         self.assertFalse(self.login.valid_user)
 
-        self.assertTrue(self.login.log_in(self.valid_id, self.valid_pw))
-        self.assertTrue(self.login._valid_user)
+        self.login.host = self.valid_url
+        self.login.connect_host()
+        self.login.user_id = self.valid_id
+        self.login.user_pw = self.valid_pw
+        self.login.log_in()
+        self.assertTrue(self.login.valid_user)
+        self.assertTrue(self.login.log_in())
 
     def test_log_out(self):
-        self.login.connect_host(self.valid_url)
-        self.login.log_in(self.valid_id, self.valid_pw)
+        self.login.host = self.valid_url
+        self.login.connect_host()
+        self.login.user_id = self.valid_id
+        self.login.user_pw = self.valid_pw
+        self.login.log_in()
+
         self.login.log_out()
         self.assertIsNone(self.login._user)
+        self.assertTrue(self.login.log_out())
 
     def test_access_setting(self):
         self.login.access_setting()
         self.assertTrue(os.path.exists(self.login.dir_path))
-        self.assertFalse(os.path.exists(self.login.user_path))
+        self.assertTrue(os.path.exists(self.login.user_path))
 
     def test_load_setting(self):
-        invalid_dict = {
-            'host': self.invalid_url,
-            'user_id': self.invalid_id,
-            'user_pw': self.invalid_pw,
-            'valid_host': False,
-            'valid_user': False,
-        }
-        with open(self.login.user_path, 'w') as json_file:
-            json.dump(invalid_dict, json_file)
-
-        self.login.load_setting()
-        self.assertFalse(self.login.valid_host)
-        self.assertFalse(self.login.valid_user)
-
-        valid_dict = {
-            'host': self.valid_url,
-            'user_id': self.valid_id,
-            'user_pw': self.valid_pw,
-            'valid_host': True,
-            'valid_user': True,
-        }
-        with open(self.login.user_path, 'w') as json_file:
-            json.dump(valid_dict, json_file)
-
-        self.login.load_setting()
-        self.assertTrue(self.login.valid_host)
-        self.assertTrue(self.login.valid_user)
+        load = self.login.load_setting()
+        self.assertIsInstance(load, dict)
 
     def test_save_setting(self):
-        user_dict = {
-            'host': self.login.host,
-            'user_id': self.login._user_id,
-            'user_pw': self.login._user_pw,
-            'valid_host': self.login.valid_host,
-            'valid_user': self.login.valid_user,
-        }
-
-        with open(self.login.user_path, 'r') as json_file:
-            user_dict = json.load(json_file)
-
-        self.login.save_setting()
+        save = self.login.save_setting()
+        self.assertIsInstance(save, dict)
 
     def test_reset_setting(self):
-        self.assertIsNone(self.login._host)
-        self.assertIsNone(self.login._user_id)
-        self.assertIsNone(self.login._user_pw)
-        self.assertFalse(self.login._valid_host)
-        self.assertFalse(self.login._valid_user)
-
-
+        self.assertIsNone(self.login.host)
+        self.assertIsNone(self.login.user_id)
+        self.assertIsNone(self.login.user_pw)
+        self.assertFalse(self.login.valid_host)
+        self.assertFalse(self.login.valid_user)
+        self.assertFalse(self.login.auto_login)

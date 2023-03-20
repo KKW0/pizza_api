d3ae0a37c359a39423191780321ef7499dc3c3c8
Ahyeon Jo <keiel0326@gmail.com>
2023-03-20 10:38:01 +0900

    dev 받아주세요

--- PizzaMaya/0.1.2/python/PizzaMaya/maya_setup/PizaMenu.py
+++ PizzaMaya/0.1.2/python/PizzaMaya/maya_setup/PizaMenu.py
@@ -1,7 +1,7 @@
 import maya.cmds as mc
 import sys
 
-sys.path.append('/home/rapa/git/pizza/python')
+sys.path.append('/home/rapa/TEST/git/pizza/PizzaMaya/0.1.2/python')
 
 if mc.menu('pizzaMenu',exists=True):
     mc.menu('pizzaMenu', e=True, dai=True)
@@ -12,6 +12,6 @@ else:
 mc.setParent(menu=True)
     
 mc.menuItem(l="Pizza", sm=True, to=True)
-mc.menuItem(l="Pizza_API", c="import Maya_Api; app = UI_controller.MainWindow();", ann="Open the Pizza.", image="pizza_API.jpg")
+mc.menuItem(l="Pizza_API", c="import PizzaMaya; app = UI_controller.MainWindow();", ann="Open the Pizza.", image="pizza_API.jpg")
 mc.setParent(menu=True)
 reload(Maya_Api)

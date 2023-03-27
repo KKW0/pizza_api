2107b5fdb40a935d1e8f81ad857a9458965c0003
Jeongtae Park <zhvkzk3@gmail.com>
2023-03-27 17:41:35 +0900

    use maya,usersetup

--- python/PizzaMaya/maya_setup/PizzaMenu.py
+++ python/PizzaMaya/maya_setup/PizzaMenu.py
@@ -15,6 +15,11 @@ mc.menuItem(l="Pizza", sm=True, to=True)
 mc.menuItem(l="Pizza_API", c="import PizzaMaya.ui.UI_controller; app = UI_controller.MainWindow();", ann="Open the Pizza.", image="pizza_API.jpg")
 mc.setParent(menu=True)
 
-shelf_name = 'pizza'
+shelf_name = "pizza"
 mc.shelfLayout(shelf_name, p="ShelfLayout")
-mc.shelfButton(label="Pizza_API", image="pizza_API.jpg", command="import PizzaMaya.ui.UI_controller; app = UI_controller.MainWindow();", parent=shelf_name)
+mc.shelfButton(
+    label="Pizza_API",
+    image="pizza_API.png",
+    command="import PizzaMaya.ui.UI_controller; myapp = PizzaMaya.ui.UI_controller.MainWindow();", 
+    parent=shelf_name
+)
